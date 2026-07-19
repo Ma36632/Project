09956af5a29c95b260d5aaa9d074723e41897866
Ma36632 <maryumaftab007@gmail.com>
2026-07-19 23:46:53 +0500

    Create App.jsx

---
+++ Ap/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="landing-page">
+      <h1>Paradise Nursery</h1>
+      <p>Welcome to our online plant nursery.</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
