c5c42f7b5351648a0c6ee07043b0483488ddc8db
Ma36632 <maryumaftab007@gmail.com>
2026-07-19 23:41:50 +0500

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,13 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Our Company</h1>
+      <p>
+        We are a software company providing innovative web solutions
+        and creating user-friendly digital experiences.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
