fea81e299bf2e24e450d3ae75ed562201961e96c
Ma36632 <maryumaftab007@gmail.com>
2026-07-21 00:05:30 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,10 +1,63 @@
+import "./App.css";
+
+import AboutUs from "./components/AboutUs";
+import ProductList from "./components/ProductList";
+import CartItem from "./components/CartItem";
+import heroImg from "./assets/hero.png";
+
 function App() {
-  return (
-    <div>
+const handleGetStarted = () => {
+document.getElementById("products").scrollIntoView({
+behavior: "smooth",
+});
+};
+
+return (
+<>
+{/* Landing Page */} <section id="center" className="landing-page"> <div className="hero"> <img
+         src={heroImg}
+         className="base"
+         width="170"
+         height="179"
+         alt="Paradise Nursery"
+       /> </div>
+
+```
+    <div className="landing-content">
       <h1>Paradise Nursery</h1>
-      <button>Get Started</button>
+
+      <p>
+        Bring nature into your home with beautiful and healthy plants.
+      </p>
+
+      <button
+        type="button"
+        className="get-started"
+        onClick={handleGetStarted}
+      >
+        Get Started
+      </button>
     </div>
-  );
+  </section>
+
+  {/* About Us */}
+  <section id="about">
+    <AboutUs />
+  </section>
+
+  {/* Product Listing */}
+  <section id="products">
+    <ProductList />
+  </section>
+
+  {/* Shopping Cart */}
+  <section id="cart">
+    <CartItem />
+  </section>
+</>
+```
+
+);
 }
 
 export default App;
