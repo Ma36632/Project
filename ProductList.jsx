93e839ed6713c26cc2747ea5ce834340c3527020
Ma36632 <maryumaftab007@gmail.com>
2026-07-20 23:26:11 +0500

    Modify product prices and add quantity to addItem
    Updated plant prices and added quantity to cart items.

--- ProductList.jsx
+++ ProductList.jsx
@@ -1,31 +1,34 @@
-import React from "react";
 import { useDispatch } from "react-redux";
 import { addItem } from "./cartSlice";
 
 function ProductList() {
-  const dispatch = useDispatch();
 
-  const plants = [
-    { id: 1, name: "Aloe Vera", price: 15 },
-    { id: 2, name: "Snake Plant", price: 20 },
-    { id: 3, name: "Peace Lily", price: 25 }
-  ];
+const dispatch = useDispatch();
 
-  return (
-    <div>
-      <h1>Paradise Nursery Plants</h1>
+const plants=[
+ {id:1,name:"Aloe Vera",price:10},
+ {id:2,name:"Snake Plant",price:15},
+ {id:3,name:"Peace Lily",price:20}
+];
 
-      {plants.map((plant) => (
-        <div key={plant.id}>
-          <h3>{plant.name}</h3>
-          <p>${plant.price}</p>
-          <button onClick={() => dispatch(addItem(plant))}>
-            Add to Cart
-          </button>
-        </div>
-      ))}
-    </div>
-  );
+return (
+<div>
+<h1>Plants</h1>
+
+{plants.map((plant)=>(
+<div key={plant.id}>
+<h3>{plant.name}</h3>
+<p>${plant.price}</p>
+
+<button onClick={()=>dispatch(addItem({...plant,quantity:1}))}>
+Add to Cart
+</button>
+
+</div>
+))}
+
+</div>
+);
 }
 
 export default ProductList;
